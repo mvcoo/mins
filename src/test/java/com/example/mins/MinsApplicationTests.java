22b7274b81cb65813357ccc08dc945c71638c9da
shark <990805016@qq.com>
2021-08-03 23:40:48 +0800

    init

---
+++ src/test/java/com/example/mins/MinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
